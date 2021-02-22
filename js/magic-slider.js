41834da90f127bc75c01c1f8724aef53c34f51de
Yuldash Atajanov <cybergod1123@gmail.com>
2021-02-23 02:25:20 +0300

    первый слайдер заработал

--- js/magic-slider.js
+++ js/magic-slider.js
@@ -1,83 +1,107 @@
 (function () {
 
-
     let m = {};
 
-    let Slider = function (props) {
+    let Slider = function (selector, props) {
         this.currentIndex = 0;//индекс текущего элменета
         this.deltaX = 0;//ширина самого длинного слайла с учетом горизонтального отступа
         this.slides = [];//массив слайдов
-        this.viewport = '<div class="magic-viewport"></div>';//то место будут хранится слайдеры
-
+        this.viewport = null;
+        this.mh = 0;
+        this.mv = 0;
+        this.sliderPadding = 0;
+        this.viewportWidth = 0;
+        this.viewportHeight = 0;
+        this.slider = $(selector);
+        this.breakpoints = [];//список медиа запросов
+        this.maxSlidingNum=0;//максимальное число перелистований
         // параметрические свойства
         this.prev = $('<button class="magic-button magic-prev-button">prev</button>');//кнпока назад
         this.next = $('<button class="magic-button magic-next-button">next</button>')//кнопка вперед
-        this.infinite = false;
+        this.infinite = false; //бесконечное прокручивание
         this.slideToShow = 1;//количество которые нужно показать в окне
         this.slideToScroll = 1;//на сколько слайдов скролить
-        this.buttons = true;
-        this.dots = false;
-        this.dotbox=null;
-        this.dotList = [];
-        this.dotItemClass = "magic-dots-item";
-        this.activedotItemClass = "magic-dot-activ";
+        this.buttons = true; //показывать или не показывать кнопки
+        this.dots = false; //показывать или не покзывать точки
+        this.dotbox = null; //контэйнер в котором хранятся скролл штуки
+        this.dotList = []; //список точек
+        this.dotItemClass = "magic-dots-item"; //стиль точки
+        this.activedotItemClass = "magic-dot-activ"; //стиль активной точки
         this.onScroll = null; //функция срабатывающая при скролле слайдов
-        // перебор всех заданных свойств аргумента
-        if (props) {
-            for (let key of Object.keys(props)) {
-                switch (key) {
-                    case 'prevButton': {
-                        this.prev = props[key];
-                        break;
-                    }
-                    case 'nextButton': {
-                        this.next = props[key];
-                        break;
-                    }
-                    case 'slideToShow': {
-                        this.slideToShow = props[key]
-                        break;
-                    }
-                    case 'slideToScroll': {
-                        this.slideToScroll = props[key];
-                        break;
-                    }
-                    case 'hasButton': {
-                        this.buttons = props[key];
-                        break;
-                    }
-                    case 'dots': {
-                        this.dots = props[key];
-                        break;
-                    }
-                    case 'dotBox':{
-                        this.dotbox=props[key];
-                        break;
-                    }
-                    case 'onScroll': {
-                        this.onScroll = props[key];
-                        break;
-                    }
-                    case 'breakpoint':{
-                        init(this,props[key].media,props[key].props);
-                        break;
-                    }
+        
+
+    }
+    Slider.prototype.setProps = function (props) {
+        
+        for (let key of Object.keys(props)) {
+            switch (key) {
+                case 'prevButton': {
+                    this.prev = props[key];
+                    break;
+                }
+                case 'nextButton': {
+                    this.next = props[key];
+                    break;
+                }
+                case 'slideToShow': {
+                    this.slideToShow = props[key]
+                    break;
+                }
+                case 'slideToScroll': {
+                    this.slideToScroll = props[key];
+                    break;
+                }
+                case 'hasButton': {
+                    this.buttons = props[key];
+                    break;
+                }
+                case 'dots': {
+                    this.dots = props[key];
+                    break;
+                }
+                case 'dotBox': {
+                    this.dotbox = props[key];
+                    break;
+                }
+                case 'onScroll': {
+                    this.onScroll = props[key];
+                    break;
+                }
+                case 'breakpoints': {
+                    this.breakpoints=props[key];
+                    break;
                 }
             }
         }
+    }
+    Slider.prototype.destroy = function () {
+        //удаление слайдера но при этом сохраняется просто верстка
+        this.slider.append($('.magic-slide'));
+        $('.magic-d').remove();
+        this.slider.children().removeClass('magic-slide').attr('style', '');
+        this.slider.removeClass('magic-slider-container').attr('style', '');
+        this.slides = [];
     }
 
 
     // создание слайдера
     m.create = function (selector, props) {
+        let s = new Slider(selector, props);//обьект слайдера
+        s.setProps(props);
+        //инициализация слайдера
+        init(s);
+        //обработчки событий
+        setHandles(s);
 
+        // check media querys
+        for (let q of s.breakpoints) {
+            checkPoint(s, q.media, q.props);
+        }
+    }
 
-
-        let s = new Slider(props);//обьект слайдера
-
-        let maxwidth = 0, maxheight = 0; //слайд с максимальной шириной и высотой
-        let querySlides = $(selector).addClass('magic-slider-container').children().addClass('magic-slide').wrapAll(s.viewport).each(function (index, el) {
-
+    function init(s) {
+        let maxwidth = 0, maxheight = 0; //велечины с максимальной шириной и высотой слайда
+        let querySlides = s.slider.addClass('magic-slider-container').children().addClass('magic-slide').wrapAll('<div class="magic-d magic-viewport"></div>').each(function (index, el) {
             //тут идет поиск самого большого слайда без учета маргинов
             let mw = parseFloat($(el).css('width'));
             let mh = parseFloat($(el).css('height'));
@@ -93,46 +117,59 @@
         if (s.slideToScroll > s.slideToShow) {
             s.slideToScroll = s.slideToShow;
         }
-
-        let itemMarginX = parseFloat(querySlides.css('margin-left'));
-        let sliderpaddingX = parseFloat($(selector).css('padding-left'));
-        let sliderpaddingY = parseFloat($(selector).css('padding-top'));
-        //длина на которую должный смещаться по оси х слайды
-        querySlides.css('margin', '0px');//обнуление всех маргинов
-        s.deltaX = itemMarginX + maxwidth + sliderpaddingX;
-
-        //задание ширины и высоты слайдера как размеры самого большого слайда в контэйнере
-
-        $(selector).css({
-            'width': s.slideToShow * maxwidth + (s.slideToShow + 1) * sliderpaddingX + 'px',
-            'height': maxheight + 2 * sliderpaddingY + 'px'
+        s.viewport = $('.magic-viewport');  //viewport
+        s.mh = parseFloat(querySlides.css('margin-right'));//горизонтальные отступы в margin слайдов
+        s.mv = parseFloat(querySlides.css('margin-top'));//вертикальные отступы в margin слайдов
+        s.sliderPadding = parseFloat(s.slider.css('padding-left'));//внутренний оступ слайдера
+        s.viewportWidth = s.slideToShow * maxwidth + (s.slideToShow - 1) * s.mh + s.mh; //вычисленная ширина vfiewport
+        s.viewportHeight = 2 * s.mv + maxheight;//вычисление высоты viewport
+        //установка размеров слайдера
+        s.slider.css({
+            'width': (s.viewportWidth + 2 * s.sliderPadding) + 'px',
+            'height': (s.viewportHeight + 2 * s.sliderPadding) + 'px'
         })
-        
+        //установка размеров viewport
+        s.viewport.css({
+            'left': s.sliderPadding + 'px',
+            'top': s.sliderPadding + 'px',
+            'width': s.viewportWidth + 'px',
+            'height': s.viewportHeight + 'px'
+        })
+        querySlides.css('margin', '0px');
+        s.deltaX = maxwidth + s.mh;
+        //позицинирование слайдов
+        let x = s.mh / 2;
+        for (let i = 0; i < s.slides.length; i++) {
+            s.slides[i].css({
+                'left': x + 'px',
+                'top': s.mv + 'px'
+            })
+            x += s.deltaX;
+        }
+        //подсчет максимального количества слайдингов
+        let tmp=s.slides.length-s.slideToShow;
+        s.maxSlidingNum=((tmp-tmp%s.slideToScroll)/s.slideToScroll)+((tmp%s.slideToScroll===0)?0:1);
+        console.log(s.maxSlidingNum);
         //добавление кнопок
         if (s.buttons) {
-            $(selector).append(s.prev).append(s.next);
+            s.slider.append(s.prev.addClass('magic-d')).append(s.next.addClass('magic-d'));
         }
-
         //добавление доттеров
-        if(s.dotbox!=null){
-            let dotcontainer = $('<div class="magic-dots-box"></div>').css({
-                'width': $(selector).css('width')
-            });
-            dotcontainer.append(s.dotbox);
-            $(selector).after(dotcontainer);
+        if (s.dotbox != null) {
+            s.slider.after(s.dotbox.addClass('magic-d'));
         }
         if (s.dots) {
-            let dotcontainer = $('<div class="magic-dots-box"></div>').css({
-                'width': $(selector).css('width')
+            let dotcontainer = $('<div class="magic-d magic-dots-box"></div>').css({
+                'width': s.slider.css('width')
             });
-            s.dotbox=$('<div class="magic-dots-list"></div>');
+            s.dotbox = $('<div class="magic-dots-list"></div>');
             for (let i = 0; i < s.slides.length; i++) {
-                let dotitem = $('<div class="' + s.dotItemClass + '"></div>');
+                let dotitem = $('<div class="magic-d ' + s.dotItemClass + '"></div>');
                 s.dotList.push(dotitem);
                 s.dotbox.append(dotitem);
             }
             dotcontainer.append(s.dotbox);
-            $(selector).after(dotcontainer);
+            s.slider.after(dotcontainer);
             s.dotList[0].addClass(s.activedotItemClass);
             if (s.onScroll == null) {
                 s.onScroll = function (index) {
@@ -143,14 +180,9 @@
                 }
             }
         }
-
-        //позицинирование слайдов
-        let x = 0;
-        for (let i = 0; i < s.slides.length; i++) {
-            s.slides[i].css('left', x + 'px')
-            x += s.deltaX;
-        }
-
+    }
+    function setHandles(s) {
+        this.currentIndex = 0;
         s.prev.on('click', function () {
             if (!s.infinite) {
 
@@ -163,14 +195,12 @@
                     countScroll = s.slideToScroll + index;
                     s.currentIndex = 0;
                 }
-                console.log("prev " + s.currentIndex)
-                console.log("prev count" + countScroll)
                 for (let i = 0; i < s.slides.length; i++) {
                     s.slides[i].animate({
                         left: parseFloat(s.slides[i].css('left')) + countScroll * s.deltaX
                     }, 300);
                 }
-                if(s.onScroll!=null) s.onScroll(s.currentIndex);
+                if (s.onScroll != null) s.onScroll(s.currentIndex,s);
             }
         })
         s.next.on('click', function () {
@@ -190,17 +220,26 @@
                         left: parseFloat(s.slides[i].css('left')) - countScroll * s.deltaX
                     }, 300);
                 }
-                if(s.onScroll!=null) s.onScroll(s.currentIndex);
+                if (s.onScroll != null) s.onScroll(s.currentIndex,s);
             }
         })
-
+    }
+    function checkPoint(s, m, prop) {
+        let media = matchMedia(m);
+        function mediaHandle(e) {
+            if (e.matches) {
+                s.destroy();
+                if(prop!=undefined) s.setProps(prop);
+                init(s);
+                console.log('initing');
+                setHandles(s);
+            }
+        }
+        media.addListener(mediaHandle)
+        mediaHandle(media);
 
     }
 
-    function init(){
-        
-    }
-    
 
     window.Magic = m;
 
--- js/script.js
+++ js/script.js
@@ -1,18 +1,53 @@
 (() => {
 
-    // const media650=matchMedia('(max-width: 650px)');
-
+    //создание первого слайдера
+    let lastIndex=0;
     Magic.create('.cart-slider', {
         slideToScroll: 1,
         slideToShow: 2,
         nextButton: $('<button class="slide-btn next"></button>'),
         prevButton: $('<button class="slide-btn prev"></button>'),
-        breakpoint: {
-            media: '(max-width: 650px)',
-            props: {
-                slideToShow: 1
+        dotBox: $('<div class="scroll-track"><div class="scroll-item"></div></div>'),
+            onScroll: function (index,slider) {
+                if(lastIndex!=index){
+                    let item=$('.scroll-item');
+                    let size=parseFloat(item.css('width'));
+                    let x=parseFloat($('.scroll-track').css('width'))/slider.maxSlidingNum;
+
+                    item.css({
+                        'left':parseFloat(item.css('left'))+((lastIndex<index)?+x-size:-x+size)+'px'
+                    })
+                    lastIndex=index;
+                }
+            },
+        breakpoints:[
+            {
+                media: '(max-width: 767px)'
+            },
+            {
+                media: '(max-width: 476px)',
+                props: {
+                    slideToShow: 1,
+                    slideToScroll: 1,
+                    onScroll: function (index,slider) {
+                        if(lastIndex!=index){
+                            let item=$('.scroll-item');
+                            let size=parseFloat(item.css('width'));
+                            let x=parseFloat($('.scroll-track').css('width'))/slider.maxSlidingNum;
+        
+                            item.css({
+                                'left':parseFloat(item.css('left'))+((lastIndex<index)?+x:-x)+'px',
+                                'transform':'translate(-50%, -50%)'
+                            })
+                            lastIndex=index;
+                        }
+                    },
+                }
             }
-        }
+        ] 
+    })
+    $('.slide-btn').bind('click',function(){
+        console.log('hhh');
     })
 
 
@@ -31,7 +66,7 @@
         }
     })
 
-    function closeThemes(t){
+    function closeThemes(t) {
         t.animate({
             opacity: 0,
             top: '400%'
@@ -39,7 +74,7 @@
             t.css('display', 'none')
         });
     }
-    function openThemes(){
+    function openThemes() {
         $('.themes-list').css('display', 'block').animate({
             opacity: 1,
             top: '70%'
@@ -49,11 +84,11 @@
         openThemes();
     })
     $('.themes-list').on('mouseleave', function (event) {
-        if($(event.relatedget)!=$('.switch-theme')){
+        if ($(event.relatedget) != $('.switch-theme')) {
             closeThemes($(this));
         }
     })
-    $('.theme-item').on('click',function(event){
+    $('.theme-item').on('click', function (event) {
         $('.switch-theme>span').text($(this).text());
         closeThemes($('.themes-list'));
     })
